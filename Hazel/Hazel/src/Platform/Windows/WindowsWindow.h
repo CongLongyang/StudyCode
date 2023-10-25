1f2a004e1ab5c4540db2e2e2c8155bfdcdc27675
cly_MSLapTop <408503704@qq.com>
2023-10-26 00:29:56 +0800

    Added OpenGL context class.

--- Hazel/Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/Hazel/src/Platform/Windows/WindowsWindow.h
@@ -2,6 +2,7 @@
 #include "Hazel/Window.h"
 
 #include <GLFW/glfw3.h>
+#include "Hazel/Renderer/GraphicsContext.h"
 
 namespace Hazel {
 
@@ -27,6 +28,7 @@ namespace Hazel {
 		virtual void Shutdown();
 	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context;
 
 		struct WindowData
 		{
