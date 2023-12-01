0e0aa796a9684fd7b05323d5a91eede99304832e
momotaro <408503704@qq.com>
2023-12-01 23:53:34 +0800

    Added Timestep and moved sandbox code to Sandbox

--- Hazel/Hazel/src/Hazel.h
+++ Hazel/Hazel/src/Hazel.h
@@ -7,6 +7,7 @@
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
 
+#include "Hazel/Core/Timestep.h"
 
 #include "Hazel/Input.h"
 #include "Hazel/KeyCodes.h"
@@ -14,6 +15,18 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+// ---Renderer------------------------
+#include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/RenderCommand.h"
+
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/VertexArray.h"
+
+#include "Hazel/Renderer/OrthographicCamera.h"
+// ----------------------------------- 
+// 
+// 
 //---------Entry Point---------------------
 #include "Hazel/EntryPoint.h"
 //---------Entry Point---------------------
